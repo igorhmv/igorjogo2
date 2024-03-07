f2782d66378989fe76911fe5191d7954de5109c3
igorhmv <125670751+igorhmv@users.noreply.github.com>
2024-03-07 10:23:19 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,31 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-app-base',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    }
+  },
+  plugins:{
+  LiveUpdates: {
+    appId: '38be06eb',
+    channel: 'Production',
+    autoUpdateMethod: 'background',
+    maxVersions: 2
+  }
+},
+};
+
+export default config;
